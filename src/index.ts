928621b4980f820ed2e99f60eb09c63192886a25
Mark Brooks <markabrooks@gmail.com>
2024-08-13 17:30:06 -0600

    add Mappable interface and add Marker

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,12 @@
 /// <reference types="@types/google.maps" />
 
-// import { User } from './User'
-// import { Company } from './Company'
+import { User } from './User'
+import { CustomMap } from './CustomMap'
+import { Company } from './Company'
 
-// const user = new User()
-// console.log(user)
+const user = new User()
+const company = new Company()
 
-// const company = new Company()
-// console.log(company)
-
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: { lat: 0, lng: 0 },
-})
+const customMap = new CustomMap('map')
+customMap.addMarker(user)
+customMap.addMarker(company)
